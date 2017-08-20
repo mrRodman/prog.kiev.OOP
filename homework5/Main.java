f48fdb23ccef056dee7c4f9c31146b3fe364c2fe
mrRodman <orix.pesh.com@gmail.com>
2017-08-20 23:21:42 +0300

    Update Main.java

--- homework5/Main.java
+++ homework5/Main.java
@@ -3,14 +3,14 @@ import java.io.*;
 public class Main {
 
 	public static void main(String[] args) {
-//		File in = new File("E:/Download/Iron Maiden - The Book of Souls (2015)/Disc 1/");
-//		File out = new File("E:/Java/");
-//		try {
-//			FileOperations.copyWithSomeExtension(in, out, "mp3");
-//		}
-//		catch (IOException e) {
-//			e.printStackTrace();
-//		}
+		File in = new File("E:/Download/Iron Maiden - The Book of Souls (2015)/Disc 1/");
+		File out = new File("E:/Java/");
+		try {
+			FileOperations.copyWithSomeExtension(in, out, "mp3");
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 
 		File in1 = new File("E:/1.txt");
 		File in2 = new File("E:/2.txt");
