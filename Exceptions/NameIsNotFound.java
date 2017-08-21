7af468de71239089bd475e76736b0cbeacbfe2dc
mrRodman <orix.pesh.com@gmail.com>
2017-08-22 01:22:42 +0300

    Delete NameIsNotFound.java

--- Exceptions/NameIsNotFound.java
+++
@@ -1,9 +0,0 @@
-package students.Exceptions;
-
-import java.util.concurrent.ExecutionException;
-
-public class NameIsNotFound extends ExecutionException {
-    public void printMessage() {
-        System.err.println("Name is not found");
-    }
-}
