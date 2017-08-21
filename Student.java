79fd015f038ded1a01f35a0b9ad52aebb1001d32
mrRodman <orix.pesh.com@gmail.com>
2017-08-22 01:12:39 +0300

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,30 @@
+package students;
+
+public class Student extends Human {
+
+	private double rating;
+
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Student(String name, int age, double rating) {
+		super(name, age);
+		this.rating = rating;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s\t%s\t%s\n", this.getName(), this.getAge(), this.getRating());
+	}
+}
