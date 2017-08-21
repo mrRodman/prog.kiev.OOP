0b401ad99f2db4cffc03193f57a6446169155a9d
mrRodman <orix.pesh.com@gmail.com>
2017-08-22 01:25:28 +0300

    Delete VoenkomMan.java

--- homework5/VoenkomMan.java
+++
@@ -1,9 +0,0 @@
-package students;
-
-import java.util.Arrays;
-
-public class VoenkomMan {
-    public void getPrivates (Voenkom voenkom) {
-        System.out.println("Get over here!" + Arrays.toString(voenkom.potentialPrivates()));
-    }
-}
